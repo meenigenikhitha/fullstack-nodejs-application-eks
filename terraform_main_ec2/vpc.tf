fd2952ff3ad7a7cc83f1b6d8ec52f866d0bfc709
meenigenikhitha <meenigenikhitha@gmail.com>
2025-05-29 17:16:51 +0530

    asdfgh

--- terraform_main_ec2/vpc.tf
+++ terraform_main_ec2/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true  # Enable DNS resolution
+  enable_dns_hostnames = true
 
   tags = {
     Name = var.vpc-name
@@ -17,7 +19,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -27,7 +29,7 @@ resource "aws_subnet" "public-subnet1" {
 resource "aws_subnet" "public-subnet2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 
   tags = {
